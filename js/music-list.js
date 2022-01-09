fd389b1aa8e1919cf23b8238b5028391299cf16d
Swasti <swastiofficial2001@gmail.com>
2022-01-09 19:23:18 +0530

    All files added

---
+++ js/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "Who I am",
+    artist: "Anne-Marie",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Infinity",
+    artist: "Jaymes Young",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Who says",
+    artist: "Selena Gomez",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "The Nights",
+    artist: "Avicii",
+    img: "music-4",
+    src: "music-4"
+  },
+];
